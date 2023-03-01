d8d41443d5755377b0128a19beade702265ebe9a
FadyGrAb <fady.michel@outlook.com>
2023-03-01 15:26:44 +0000

    week02, rollbar. Done

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -31,6 +31,11 @@ import watchtower
 import logging
 from time import strftime
 
+# Rollbar imports
+import rollbar
+import rollbar.contrib.flask
+from flask import got_request_exception
+
 app = Flask(__name__)
 
 
@@ -57,6 +62,10 @@ cw_handler = watchtower.CloudWatchLogHandler('cruddur')
 LOGGER.addHandler(console_handler)
 LOGGER.addHandler(cw_handler)
 LOGGER.info("test-log")
+
+# Init Rollbar
+rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
+
 
 frontend = os.getenv('FRONTEND_URL')
 backend = os.getenv('BACKEND_URL')
@@ -168,6 +177,27 @@ def after_request(response):
     timestamp = strftime('[%Y-%b-%d %H:%M]')
     LOGGER.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
     return response
+
+@app.before_first_request
+def init_rollbar():
+    """init rollbar module"""
+    rollbar.init(
+        # access token
+        rollbar_access_token,
+        # environment name
+        'production',
+        # server root directory, makes tracebacks prettier
+        root=os.path.dirname(os.path.realpath(__file__)),
+        # flask already sets up logging
+        allow_logging_basic_config=False)
+
+    # send exceptions from `app` to rollbar, using flask's signal system.
+    got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
+
+@app.route('/rollbar/test')
+def rollbar_test():
+    rollbar.report_message('Hello World!', 'warning')
+    return "Hello World!"
 
 if __name__ == "__main__":
   app.run(debug=True)
--- journal/week2.md
+++ journal/week2.md
@@ -20,4 +20,10 @@ I was able to instrument AWS X-Ray as follows:
 
 ### Configure custom logger to send to CloudWatch Logs:
 I was able to send the application logs to CloudWatch:  
-![CloudWatch](assests/week02/req-hw-cloudwatch-01.png)
+![CloudWatch](assests/week02/req-hw-cloudwatch-01.png)  
+
+### Integrate Rollbar and capture an error:
+I have integrated Rollbar and received the warning
+![rollbar warning](assests/week02/req-hw-rollbar-01.png)  
+And received an error (commented out the return from HomeActivities run function)  
+![rollbar error](assests/week02/req-hw-rollbar-02.png)  
