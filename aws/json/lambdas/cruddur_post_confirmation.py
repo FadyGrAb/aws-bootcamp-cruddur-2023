ec87852a4a0ca6fbb6733114b5bbfb8efd094dda
FadyGrAb <fady.michel@outlook.com>
2023-03-15 07:09:47 +0000

    week04 post confirmation lambda fixed

--- aws/json/lambdas/cruddur_post_confirmation.py
+++ aws/json/lambdas/cruddur_post_confirmation.py
@@ -13,18 +13,18 @@ def lambda_handler(event, context):
     try:
         sql = f"""
             INSERT INTO public.users (display_name, email, handle, cognito_user_id) 
-            VALUES(%s, %s, %s, %s)
+            VALUES(%(user_display_name)s, %(user_email)s, %(user_handle)s, %(user_cognito_id)s)
         """
 
         conn = psycopg2.connect(os.getenv("CONNECTION_URL"))
         cur = conn.cursor()
-        params = [
-            user_display_name,
-            user_email,
-            user_handle,
-            user_cognito_id
-        ]
-        cur.execute(sql, *params)
+        params = {
+            "user_display_name": user_display_name,
+            "user_email": user_email,
+            "user_handle": user_handle,
+            "user_cognito_id": user_cognito_id
+        }
+        cur.execute(sql, params)
         conn.commit() 
 
     except (Exception, psycopg2.DatabaseError) as error:
--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,7 +1,7 @@
-INSERT INTO public.users (display_name, handle, cognito_user_id)
+INSERT INTO public.users (display_name, handle, email, cognito_user_id)
 VALUES
-  ('Andrew Brown', 'andrewbrown' ,'MOCK'),
-  ('Andrew Bayko', 'bayko' ,'MOCK');
+  ('Andrew Brown', 'andrewbrown' , 'brown@mail.com', 'MOCK'),
+  ('Andrew Bayko', 'bayko' ,'bayko@mail.com', 'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
