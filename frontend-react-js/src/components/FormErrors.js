c3c6afac3e54054a6e03b9f0350635373dd7a592
FadyGrAb <fady.michel@outlook.com>
2023-05-30 07:48:28 +0000

    week-x fix signin error message

--- frontend-react-js/src/components/FormErrors.js
+++ frontend-react-js/src/components/FormErrors.js
@@ -2,12 +2,16 @@ import './FormErrors.css';
 import FormErrorItem from 'components/FormErrorItem';
 
 export default function FormErrors(props) {
-  let el_errors = null
-  if (props.errors.length > 0) {
+  let el_errors = null;
+  if ((props.errors.length > 0) && Array.isArray(props.errors)) {
     el_errors = (<div className='errors'>
       {props.errors.map((err_code, key) => {
         return <FormErrorItem err_code={err_code} key={key}/>
       })}
+    </div>)
+  } else if (props.errors.length > 0) {
+    el_errors = (<div className='errors'>
+       <FormErrorItem err_code={props.errors} key={0}/>
     </div>)
   }
 
